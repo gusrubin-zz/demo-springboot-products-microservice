11909b47d1b3e149d8191f6f3756640b9d7d5261
Gustavo Rubin <gusrubin@gmail.com>
2020-08-18 21:23:01 -0300

    Spring Boot initial structure

---
+++ src/main/java/com/gustavorubin/abinbevecommerce/AbinbevEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.gustavorubin.abinbevecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbinbevEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbinbevEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavorubin/abinbevecommerce/AbinbevEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavorubin.abinbevecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbinbevEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
